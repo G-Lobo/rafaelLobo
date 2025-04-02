db55a167483688af69e554712465c47383fbcded
Lobo <gbrilobo@gmail.com>
2025-04-01 21:27:30 -0300

    create workflow

--- resources/views/layouts/rafaelLobo.blade.php
+++ resources/views/layouts/rafaelLobo.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,20 +8,34 @@
     <link rel="icon" href="{{ asset('assets/img/icon/iconeloboPreto.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@100..900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/quill@2.0.2/dist/quill.snow.css" rel="stylesheet" />
 
 
-    <style>.font-mont {font-family:'Montserrat',sans-serif;}</style>
-    <style>.font-heebo {font-family: 'Heebo',sans-serif;}</style>
-    <style>ion-icon {font-size: 32px;}</style>
+    <style>
+        .font-mont {
+            font-family: 'Montserrat', sans-serif;
+        }
+    </style>
+    <style>
+        .font-heebo {
+            font-family: 'Heebo', sans-serif;
+        }
+    </style>
+    <style>
+        ion-icon {
+            font-size: 32px;
+        }
+    </style>
     <script src="https://cdn.tailwindcss.com"></script>
 
     <title>Rafael Lobo</title>
 
 
 </head>
+
 <body class="font-mont bg-gray-100">
     <header class="">
         @yield('header')
@@ -42,4 +57,5 @@
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
+
 </html>
