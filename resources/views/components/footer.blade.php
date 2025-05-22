f79035c206a5eb0cfa9bb741837630f761aeae72
Gabriel Lobo <108934953+G-Lobo@users.noreply.github.com>
2025-05-22 01:19:05 +0000

    teste

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,32 +1,6 @@
 <div class="flex justify-between px-6 py-6 bg-white">
 
-    <!-- Left Side Links -->
-    <div class="flex flex-col align-middle pt-5">
-        <h1 class="font-black text-2xl text-black">RAFAEL LOBO</h1>
-
-        <div>
-
-            <x-social-icons />
-
-        </div>
-
-    </div>
-
-    <!-- Right Side Links -->
-    <div class="flex flex-col items-end">
-        <h2 class="font-heebo text-xl text-black">
-            <a href="{{ route('home') }}">HOME</a>
-        </h2>
-        <h2 class="font-heebo text-xl text-black">
-            <a href="{{ route('about.index') }}">BIO</a>
-        </h2>
-        <h2 class="font-heebo text-xl text-black">
-            <a href="{{ route('blog.index') }}">FILMES</a>
-        </h2>
-        <h2 class="font-heebo text-xl text-black">
-            <a href="#">POSTS</a>
-        </h2>
-    </div>
+    TESTE
 </div>
 
 {{-- <div class="bg-[#222222] py-2">
