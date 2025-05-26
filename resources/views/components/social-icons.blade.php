e04204c3d95579c948050c0eb0e30d52426ea2df
Lobo <gbrilobo@gmail.com>
2025-05-26 09:01:09 -0300

    mudança no icone do imdb

--- resources/views/components/social-icons.blade.php
+++ resources/views/components/social-icons.blade.php
@@ -1,25 +1,23 @@
 <div class="flex justify-center space-x-5">
     <!-- Instagram -->
     <a href="https://www.instagram.com/rafael_lobo/" class="text-black hover:text-gray-300 transition duration-300" target="_blank" rel="noopener noreferrer">
-        <ion-icon name="logo-instagram">
-
-        </ion-icon>
+        <ion-icon name="logo-instagram" class="text-4xl"></ion-icon>
     </a>
 
     <!-- LinkedIn -->
     <a href="https://www.linkedin.com/in/rafael-lobo-660ab433"
         class="text-black hover:text-gray-300 transition duration-300" target="_blank" rel="noopener noreferrer">
-        <ion-icon name="logo-linkedin"></ion-icon>
+        <ion-icon name="logo-linkedin" class="text-4xl"></ion-icon>
     </a>
 
     <!-- IMDB -->
     <a href="https://www.imdb.com/name/nm3835881/?ref_=nv_sr_srsg_0_tt_0_nm_8_in_0_q_rafael%2520lobo"
         class="text-black hover:text-gray-300 transition duration-300" target="_blank" rel="noopener noreferrer">
-        <ion-icon name="star-outline"></ion-icon>
+        <img src="{{ asset("/assets/img/icon/imdb-preto.svg") }}" alt="" class="h-9">
     </a>
 
     <!-- Vimeo -->
     <a href="https://vimeo.com/rafaellobo" class="text-black hover:text-gray-300 transition duration-300" target="_blank" rel="noopener noreferrer">
-        <ion-icon name="logo-vimeo"></ion-icon>
+        <ion-icon name="logo-vimeo" class="text-4xl"></ion-icon>
     </a>
 </div>
